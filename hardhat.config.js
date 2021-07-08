c523982c1b7d8aa735ec46c7eb50f89961b0e0c3
Guillaume Dieudonné <dieug_@MacBook-Pro-de-Guillaume.local>
2021-07-08 17:57:06 +0200

    feat: initial commit before repo refacto

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,10 +22,10 @@ module.exports = {
   solidity: {
     version: "0.6.12",
     settings: {
-      // optimizer: {
-      //   enabled: true,
-      //   runs: 200
-      // }
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   },
 
